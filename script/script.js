198c9c268322711ec050cb606bf416573acb9889
48790 <48790@LAPTOP-M3HTDCPL>
2021-01-29 00:10:17 +0100

    add skills progres bar

--- script/script.js
+++ script/script.js
@@ -1,5 +1,43 @@
 const hamburger = document.querySelector('.nav-menu-button');
 const nav = document.querySelector('.nav-menu-list');
+const progressBar = document.querySelectorAll('.skills-content-single-scale');
+
+const progressEle = document.createElement("div");
+progressEle.classList.add("skills-content-single-bar");
+
+
+for(let i=0; i<10; i++){
+  const singleStep = document.createElement("div");
+  singleStep.classList.add("skills-content-single-bar-ele");
+  progressEle.appendChild(singleStep);
+}
+
+progressBar.forEach(el => {
+  el.appendChild(progressEle.cloneNode(true));
+});
+
+const one = document.getElementById('one').querySelector('.skills-content-single-bar').childNodes;
+const two = document.getElementById('two').querySelector('.skills-content-single-bar').childNodes;
+const three = document.getElementById('three').querySelector('.skills-content-single-bar').childNodes;
+const four = document.getElementById('four').querySelector('.skills-content-single-bar').childNodes;
+const five = document.getElementById('five').querySelector('.skills-content-single-bar').childNodes;
+const six = document.getElementById('six').querySelector('.skills-content-single-bar').childNodes;
+
+const changeColor = (index, lang) => {
+  for (let i=0; i<index; i++){
+    lang[i].classList.toggle('skills-content-single-bar-elealt');
+  }
+}
+
+changeColor(9, one);
+changeColor(8, two);
+changeColor(8, three);
+changeColor(5, four);
+changeColor(2, five);
+changeColor(6, six);
+
+
+
 
 const handleClick = () => {
   hamburger.classList.toggle('nav-menu-button-click');
@@ -7,3 +45,5 @@ const handleClick = () => {
 }
 
 hamburger.addEventListener('click', handleClick);
+
+
