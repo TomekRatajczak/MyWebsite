09154743a9b36e002091e80d79a6a64dc3a7b786
48790 <48790@LAPTOP-M3HTDCPL>
2021-02-05 00:17:24 +0100

    Add navigation links and scrollTo

--- script/script.js
+++ script/script.js
@@ -62,6 +62,40 @@ gsap.to(".panel:not(:last-child)", {
 
 gsap.set(".panel", {zIndex: (i, target, targets) => targets.length - i});
 
+//  ScrollTo
+gsap.registerPlugin(ScrollToPlugin);
+// Detect if a link's href goes to the current page
+function getSamePageAnchor (link) {
+  if (
+    link.protocol !== window.location.protocol ||
+    link.host !== window.location.host ||
+    link.pathname !== window.location.pathname ||
+    link.search !== window.location.search
+  ) {
+    return false;
+  }
+
+  return link.hash;
+}
+
+// Scroll to a given hash, preventing the event given if there is one
+function scrollToHash(hash, e) {
+  const elem = hash ? document.querySelector(hash) : false;
+  if(elem) {
+    if(e) e.preventDefault();
+    gsap.to(window, {scrollTo: elem});
+  }
+}
+
+// If a link's href is within the current page, scroll to it instead
+document.querySelectorAll('a[href]').forEach(a => {
+  a.addEventListener('click', e => {
+    scrollToHash(getSamePageAnchor(a), e);
+  });
+});
+
+// Scroll to the element in the URL's hash on load
+scrollToHash(window.location.hash);
 
 //OpinionSlider
 const slides = document.querySelectorAll(".opinion-carousel-single");
